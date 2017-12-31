0aed2c9633ca057d839ebb8b64803976a50726c5
MYXue <xiaoxuekl@163.com>
2017-12-31 17:04:34 +0800

    myx

--- AMC_system/AMC/extensions.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext import admin
-
-db = SQLAlchemy()
-admin = admin.Admin(name=u'AMC系统 数据库管理')
